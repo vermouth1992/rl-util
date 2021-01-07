91633587988143a292aa4b0d8360241810d0f06b
ASAU-ubuntu <czhangseu@gmail.com>
2021-01-06 21:22:59 -0800

    benchmark mujoco

---
+++ scripts/benchmark_mujoco.sh
@@ -0,0 +1,20 @@
+envs=(
+  Hopper-v2
+  Walker2d-v2
+  HalfCheetah-v2
+  Ant-v2
+)
+
+algos=(ppo trpo sac td3 ddpg)
+
+seeds=(
+  110 111 112 113 114 115 116 117 118 119
+)
+
+for env in "${envs[@]}"; do
+  for algo in "${algos[@]}"; do
+    for seed in "${seeds[@]}"; do
+      python -m rlutils.run $algo --env_name $env --seed $seed
+    done
+  done
+done
