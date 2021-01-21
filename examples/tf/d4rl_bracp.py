a9b989837e5a040414fbe4da17d98aa9e78f3188
ASAU-ubuntu <czhangseu@gmail.com>
2021-01-20 17:32:54 -0800

    update hyper parameters

--- examples/tf/d4rl_bracp.py
+++ examples/tf/d4rl_bracp.py
@@ -115,10 +115,10 @@ def train_policy(args):
             'generalization_threshold': 0.1,
         },
         'walker2d-medium-expert-v0': {
-            'generalization_threshold': 0.1,
+            'generalization_threshold': 0.2,
         },
         'halfcheetah-medium-expert-v0': {
-            'generalization_threshold': 0.1,
+            'generalization_threshold': 0.2,
         },
         'hopper-medium-v0': {
             'generalization_threshold': 0.4,
