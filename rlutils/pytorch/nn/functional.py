87529a5271173f04405ca1736a85cda60aebaf17
Chi Zhang <czhangseu@gmail.com>
2021-11-01 12:34:22 -0700

    update pytorch functional

--- rlutils/pytorch/nn/functional.py
+++ rlutils/pytorch/nn/functional.py
@@ -25,8 +25,19 @@ def decode_activation(activation):
 
 def build_mlp(input_dim, output_dim, mlp_hidden, num_ensembles=None, num_layers=3,
               activation='relu', out_activation=None, squeeze=False, dropout=None,
-              batch_norm=False):
-    assert not batch_norm, 'BatchNorm is not supported yet.'
+              batch_norm=False, layer_norm=False):
+    if num_ensembles is not None:
+        assert not batch_norm, 'BatchNorm is not supported for EnsembleDense yet.'
+    assert not (batch_norm and layer_norm), "batch_norm and layer_norm can't be True simultaneously"
+    if squeeze:
+        assert output_dim == 1, "if squeeze, output_dim must have size 1"
+    if isinstance(mlp_hidden, int):
+        mlp_hidden = [mlp_hidden] * (num_layers - 1)
+    elif isinstance(mlp_hidden, list) or isinstance(mlp_hidden, tuple):
+        assert len(mlp_hidden) == num_layers - 1, 'len(mlp_hidden) must equal to num_layers - 1.'
+    else:
+        raise ValueError(f'Unknown type mlp_hidden. Got {type(mlp_hidden)}')
+
     activation_fn = decode_activation(activation)
     output_activation_fn = decode_activation(out_activation)
     layers = []
@@ -38,28 +49,32 @@ def build_mlp(input_dim, output_dim, mlp_hidden, num_ensembles=None, num_layers=
     else:
         # first layer
         if num_ensembles is not None:
-            layers.append(EnsembleDense(num_ensembles, input_dim, mlp_hidden))
+            layers.append(EnsembleDense(num_ensembles, input_dim, mlp_hidden[0]))
         else:
-            layers.append(nn.Linear(input_dim, mlp_hidden))
+            layers.append(nn.Linear(input_dim, mlp_hidden[0]))
+            if batch_norm:
+                layers.append(nn.BatchNorm1d(num_features=mlp_hidden[0]))
         layers.append(activation_fn())
         if dropout is not None:
             layers.append(nn.Dropout(p=dropout))
 
         # intermediate layers
-        for _ in range(num_layers - 2):
+        for i in range(num_layers - 2):
             if num_ensembles is not None:
-                layers.append(EnsembleDense(num_ensembles, mlp_hidden, mlp_hidden))
+                layers.append(EnsembleDense(num_ensembles, mlp_hidden[i], mlp_hidden[i + 1]))
             else:
-                layers.append(nn.Linear(mlp_hidden, mlp_hidden))
+                layers.append(nn.Linear(mlp_hidden[i], mlp_hidden[i + 1]))
+                if batch_norm:
+                    layers.append(nn.BatchNorm1d(num_features=mlp_hidden[i + 1]))
             layers.append(activation_fn())
             if dropout is not None:
                 layers.append(nn.Dropout(p=dropout))
 
         # final dense layer
         if num_ensembles is not None:
-            layers.append(EnsembleDense(num_ensembles, mlp_hidden, output_dim))
+            layers.append(EnsembleDense(num_ensembles, mlp_hidden[-1], output_dim))
         else:
-            layers.append(nn.Linear(mlp_hidden, output_dim))
+            layers.append(nn.Linear(mlp_hidden[-1], output_dim))
 
     if out_activation is not None:
         layers.append(output_activation_fn())
