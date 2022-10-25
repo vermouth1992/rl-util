6e5562883aafe889b70fa44cffc6d2e5c3caec22
Chi Zhang <czhangseu@gmail.com>
2022-10-26 01:51:14 +0800

    mbpo

--- model_based/mbpo.py
+++ model_based/mbpo.py
@@ -4,3 +4,31 @@ Due to the high computation requirement, we employ the following process:
 - A process that samples data and performs dynamics training
 - A process that samples data to perform rollouts
 """
+
+
+class PolicyTrainer(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        while True:
+            # step 1: performs an environmental interaction
+
+            # step 1: samples a batch
+            pass
+
+
+class DynamicsTrainer(object):
+    pass
+
+
+class Rollout(object):
+    pass
+
+
+class Logger(object):
+    pass
+
+
+if __name__ == '__main__':
+    pass
